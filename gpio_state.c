1f9c1cfbea6ffec9439702201568bba7f39049b0
Oskar Wiren <owiren@kth.se>
2024-11-10 17:15:25 +0100

    Fix

--- gpio_state.c
+++ gpio_state.c
@@ -29,28 +29,29 @@ void updateButtonState(const unsigned char newState){
 unsigned char get_dpad_state() {
     unsigned char changedBtns = LAST_BUTTONSTATE ^ *((volatile int*) 0x040000e0);
     switch(changedBtns){
-    case 0b0001:
-        // höger
-        if ((LAST_BUTTONSTATE & 0b1) == 0) {
-            MOVE_DIRECTION = 1;
-        } else {
-        MOVE_DIRECTION = 0;
-        }
+        case 0b0001:
+            // höger
+            if ((LAST_BUTTONSTATE & 0b1) == 0) {
+                MOVE_DIRECTION = 1;
+            } else {
+            MOVE_DIRECTION = 0;
+            }
 
-        break;
-    case 0b0010:
-        // vänster
-        MOVE_DIRECTION = 1;
-        break;
-    case 0b0100:
-        // ner
-        MOVE_DIRECTION = 2;
-        break;
-    case 0b1000:
-        // upp
-        MOVE_DIRECTION = 3;
-        break;
-    default:
+            break;
+        case 0b0010:
+            // vänster
+            MOVE_DIRECTION = 1;
+            break;
+        case 0b0100:
+            // ner
+            MOVE_DIRECTION = 2;
+            break;
+        case 0b1000:
+            // upp
+            MOVE_DIRECTION = 3;
+            break;
+        default:
+    }
 
     return MOVE_DIRECTION;
     }
