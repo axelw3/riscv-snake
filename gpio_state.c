81d3003f77efc69117e2b037a62ae541899e3096
Oskar Wiren <owiren@kth.se>
2024-11-10 16:50:51 +0100

    Fix

--- gpio_state.c
+++ gpio_state.c
@@ -1,33 +1,27 @@
-unsigned char LAST_GPIO_BUTTONSTATE = 0b0000;
+unsigned char LAST_BUTTONSTATE = 0b0000;
+unsigned char MOVE_DIRECTION = 0;
 
-//  Returns the change for one call when change is detected. 
-//  Storage of state change is left to caller.
-//  OBS!!! state 0 is reservved for no change.
-int get_button_state_change(){
-    unsigned char move_direction = 0;
-    volatile unsigned char* current_state = (unsigned char*) 0x040000e0;
-    unsigned char changedBtns = LAST_GPIO_BUTTONSTATE ^ *current_state; // bitwise XOR
+void updateButtonState(const unsigned char newState){
+    unsigned char changedBtns = LAST_BUTTONSTATE ^ newState; // bitwise XOR
     switch(changedBtns){
         case 0b0001:
             // höger
-            move_direction = 1;
+            MOVE_DIRECTION = 0;
             break;
         case 0b0010:
             // vänster
-            move_direction = 2;
+            MOVE_DIRECTION = 1;
             break;
         case 0b0100:
             // ner
-            move_direction = 3;
+            MOVE_DIRECTION = 2;
             break;
         case 0b1000:
             // upp
-            move_direction = 4;
+            MOVE_DIRECTION = 3;
             break;
         default:
-            // no change
-            // move_direction = 0;
     }
-    LAST_GPIO_BUTTONSTATE = current_state;
-    return move_direction;
+    LAST_BUTTONSTATE = newState;
+    return;
 }
