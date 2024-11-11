befa8f52e36a01dc54fb332377faf1732a341b2d
Axel Sethsson <sethsson@kth.se>
2024-11-11 18:40:45 +0100

    VGA test

---
+++ vga.c
@@ -0,0 +1,53 @@
+/**
+ * Width, in pixels.
+*/
+const unsigned int WIDTH = 320;
+
+/**
+ * VGA backbuffer.
+*/
+volatile char *VGA = (volatile char*) 0x08000000;
+
+/**
+ * DMA controller register.
+*/
+volatile char *VGA_CTRL = (volatile int*) 0x04000100;
+
+/**
+ * Set the color of a pixel in the backbuffer.
+*/
+void setPixel(unsigned int x, unsigned int y, unsigned char color){
+    *(VGA + y * WIDTH + x) = color;
+    return;
+}
+
+/**
+ * Perform framebuffer swap.
+*/
+void swap(){
+    *(VGA_CTRL + 1) = VGA; // set address of rgb data
+    *VGA_CTRL = 0; // swap
+    return;
+}
+
+/**
+ * Draw a filled square to the backbuffer.
+*/
+void fillSquare(unsigned int x0, unsigned int y0, unsigned int w, unsigned int h, unsigned char color){
+    for(int x = 0; x < w; x++){
+        for(int y = 0; y < h; y++){
+            setPixel(x0 + x, y0 + y, color);
+        }
+    }
+    return;
+}
+
+/**
+ * Example main function. Draws a 30x30 filled square at x = 10, y = 10.
+*/
+int main(){
+    fillSquare(10, 10, 30, 30, 0xd4);
+    swap();
+
+    return 0;
+}
