6f976c78ae691e5f4eda7c048fe2432dafe67999
Oskar Wiren <owiren@kth.se>
2024-11-16 16:19:08 +0100

    Får inte main att hämta ut data från get_dpad_state.

---
+++ DPAD_STATE.h
@@ -0,0 +1,12 @@
+// DPAD_STATE_H
+#ifndef DPAD_STATE_H
+#define DPAD_STATE_H
+
+enum DPAD_STATE{
+    UP = 0b0111,
+    DOWN = 0b1011,
+    LEFT = 0b1101,
+    RIGHT = 0b1110
+};
+
+#endif // DPAD_STATE_H
